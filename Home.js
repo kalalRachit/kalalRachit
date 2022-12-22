ec9d243581609b516b5c5860afccb6b1ac32b9a0
kalalRachit <117704684+kalalRachit@users.noreply.github.com>
2022-12-22 20:57:15 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "./Layout";
+import About from "./About";
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+function Home(){
+    return(
+        <div>
+        </div>
+    )
+}
+export default Home
